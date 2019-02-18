4c0059478915b2523050e4d292a0d168d248023d
Nubbelium <43620094+Nubbelium@users.noreply.github.com>
2019-02-18 12:37:55 -0600

    Basic Engine Is Done
    First commit and first documented change, I really should have switch right off the bat

---
+++ Infinity war 0.2.5 (test 1) 4.21/Intermediate/Source/InfinityWar.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class InfinityWar : ModuleRules
+{
+	public InfinityWar(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
